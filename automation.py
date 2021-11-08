cf47baccf9e111f50a29315657401f625885f737
Virtual Kiddie <56595196+virtualkiddie@users.noreply.github.com>
2021-11-09 00:36:31 +0530

    Add files via upload
    Added GET request in automation script and respective chrome-driver

---
+++ automation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+#Driver_Path
+s=Service('/home/hoaxer/PycharmProjects/automation/chromedriver')
+#driver_path = "/home/hoaxer/PycharmProjects/automation/chromedriver"
+
+
+#Browser_Binary_Path
+brave_path = "/usr/bin/brave-browser"
+
+#Chrome_Options
+option = webdriver.ChromeOptions()
+option.binary_location = brave_path
+
+#option.add_argument("--incognito")
+#option.add_argument("--headless")
+
+# Create new Instance of Chrome
+browser = webdriver.Chrome(service=s,options=option)
+#browser = webdriver.Chrome(executable_path=driver_path, chrome_options=option)
+
+browser.get("https://shopatsc.com/")
+
